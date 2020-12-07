1434dd33f8fa38fd048f8319c954a3805a0c2471
Stefanos Leventis <sigianas@yahoo.com>
2020-12-07 23:57:56 +0200

    styles complete

---
+++ app.js
@@ -0,0 +1,8 @@
+// const buttons = document.querySelectorAll('.button');
+// const lights = document.querySelectorAll('.light');
+
+// for (let btn of buttons) {
+//     btn.addEventListener('click', () => {
+//         btn.classList.toggle('on')
+//     });
+// };
