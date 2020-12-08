de4b18966eaa32c6ce03146b7d3916d11cf8ebd4
Stefanos Leventis <sigianas@yahoo.com>
2020-12-08 23:09:12 +0200

    button keys assigned

--- app.js
+++
@@ -1,8 +0,0 @@
-// const buttons = document.querySelectorAll('.button');
-// const lights = document.querySelectorAll('.light');
-
-// for (let btn of buttons) {
-//     btn.addEventListener('click', () => {
-//         btn.classList.toggle('on')
-//     });
-// };
