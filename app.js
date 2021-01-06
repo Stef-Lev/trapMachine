5bf0d04a9e907c968b6dd9c1af9f1025b3a69a39
Stefanos Leventis <sigianas@yahoo.com>
2021-01-07 01:26:34 +0200

    added click events and refactored

--- app.js
+++ app.js
@@ -1,27 +1,37 @@
+// remove the playing animation
 function removeTransition(e) {
     if (e.propertyName !== 'transform') return;
     e.target.classList.remove('playing');
 }
 
-function playSoundDesktop(e) {
+// play sound when pressing keyboard keys
+function playOnKeyboard(e) {
+    e.stopPropagation();
     const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
     const key = document.querySelector(`div[data-key="${e.keyCode}"]`);
     if (!audio) return;
-
     key.classList.add('playing');
     audio.currentTime = 0;
     audio.play();
 }
-function playSoundMobile(e) {
-    const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
-    if (!audio) return;
 
+// play sound when clicking on buttons
+function playOnClick(e) {
+    e.stopPropagation();
+    const dataCode = parseInt(e.target.dataset.key);
+    const audio = document.querySelector(`audio[data-key="${dataCode}"]`);
+    const key = document.querySelector(`div[data-key="${dataCode}"]`);
+    if (!audio) return;
+    key.classList.add('playing');
     audio.currentTime = 0;
     audio.play();
 }
 
-const buttons = Array.from(document.querySelectorAll('.button'));
+//Event listeners
+const buttons = document.querySelectorAll('.button');
+window.addEventListener('keydown', playOnKeyboard);
 buttons.forEach(button => button.addEventListener('transitionend', removeTransition));
+buttons.forEach(button => button.addEventListener('click', playOnClick));
 
-window.addEventListener('keydown', playSoundDesktop);
-window.addEventListener('click', playSoundMobile);
+
+
